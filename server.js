07c94aed054d4075bca7b9db1f91f5a55d437221
dnlengnl12 <70835908+dnlengnl12@users.noreply.github.com>
2022-04-19 22:52:17 +0900

    SetUp: prisma, apollo, babel, database, graphql

---
+++ server.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+import { ApolloServer } from "apollo-server";
+import schema from "./schema";
+
+const server = new ApolloServer({schema});
+
+const PORT = process.env.PORT;
+
+server.listen(PORT).then(() => console.log(`http://localhost:${PORT}`));
