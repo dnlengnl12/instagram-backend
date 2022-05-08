ae219249d6df0c5cec9cb35095001fbc0ecb1121
dnlengnl12 <70835908+dnlengnl12@users.noreply.github.com>
2022-05-08 21:22:19 +0900

    Add: isLiked

--- photos/photos.typeDefs.js
+++ photos/photos.typeDefs.js
@@ -1,32 +1,33 @@
 import { gql } from "apollo-server-express";
 
 export default gql`
-  type Photo {
-    id: Int!
-    user: User!
-    file: String!
-    caption: String
-    hashtags: [Hashtag]
-    createdAt: String!
-    updatedAt: String!
-    likes: Int!
-    isMine: Boolean!
-    comments: Int!
-  }
+	type Photo {
+		id: Int!
+		user: User!
+		file: String!
+		caption: String
+		hashtags: [Hashtag]
+		createdAt: String!
+		updatedAt: String!
+		likes: Int!
+		isMine: Boolean!
+		comments: Int!
+		isLiked: Boolean!
+	}
 
-  type Hashtag {
-    id: Int!
-    hashtag: String!
-    photos(page: Int!): [Photo]
-    totalPhotos: Int!
-    createdAt: String!
-    updatedAt: String!
-  }
+	type Hashtag {
+		id: Int!
+		hashtag: String!
+		photos(page: Int!): [Photo]
+		totalPhotos: Int!
+		createdAt: String!
+		updatedAt: String!
+	}
 
-  type Like {
-    id: Int!
-    photo: Photo!
-    createdAt: String!
-    updatedAt: String!
-  }
+	type Like {
+		id: Int!
+		photo: Photo!
+		createdAt: String!
+		updatedAt: String!
+	}
 `;
